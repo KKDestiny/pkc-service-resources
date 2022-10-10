c076f3e825d053a0ed1f00aacfbafe93ce021765
linxiaozhou <1139904786@qq.com>
2022-10-11 00:01:44 +0800

    chore: 同步代码

--- scripts/gen-schemas.ts
+++ scripts/gen-schemas.ts
@@ -2,11 +2,21 @@
  * @Author: linxiaozhou.com
  * @LastEditors: linxiaozhou.com
  * @Description: 导出项目swagger数据
- */ import { run } from "../src/specs/tools/schemas.tool";
+ */ 
+import { run } from "../src/specs/tools/schemas.tool";
+import { schema as accountsSchema } from "../src/models/account.model";
+
+const schemas = [
+  {
+    filename: "accounts",
+    schemaName: "Accounts",
+    schema: accountsSchema,
+  },
+];
 
 async function exec() {
   console.log(`===================== Begin Generate Schemas =====================`);
-  await run();
+  await run(schemas);
   console.log(`====================== End Generate Schemas ======================\n\n`);
 }
 exec();
